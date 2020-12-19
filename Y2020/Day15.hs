7dc6f3596036ea8c76709a11e12fd5844d8e4fcd
Alex Bradley <a.bradley.dm@gmail.com>
2020-12-19 02:33:56 +0000

    2020 Day 15

---
+++ Y2020/Day15.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE BangPatterns #-}
+
+module AdventOfCode.Y2020.Day15 where
+
+import Data.List
+import Data.Maybe
+import Data.IntMap.Strict (IntMap)
+import qualified Data.IntMap.Strict as M
+
+import Control.DeepSeq
+
+type Term = Int
+type Sequence = [Term]
+
+getTerm :: Int -> Sequence -> Term
+getTerm n seed | n < length seed = seed !! n
+getTerm n seed = let
+   !lastIndexMap = M.fromList $ zip (init seed) [0..]
+   in aux (length seed - 1) (last seed) lastIndexMap
+   where
+   aux :: Int -> Term -> IntMap Int -> Term
+   aux idx term _ | idx == n = term
+   aux idx term lastIndexMap = let
+      !nextTerm = idx - M.findWithDefault idx term lastIndexMap
+      !lastIndexMap' = M.insert term idx lastIndexMap
+      in aux (idx+1) nextTerm lastIndexMap'
+
+part1 = getTerm (2020-1)
+part2 = getTerm (30000000-1)
+
+sample1 = [0,3,6] :: Sequence
+sample2 = [1,3,2] :: Sequence
+sample3 = [2,1,3] :: Sequence
+sample4 = [1,2,3] :: Sequence
+sample5 = [2,3,1] :: Sequence
+sample6 = [3,2,1] :: Sequence
+sample7 = [3,1,2] :: Sequence
+myInput = [2,15,0,9,1,20] :: Sequence
