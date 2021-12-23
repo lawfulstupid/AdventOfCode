7dca2614073ae9d21e1222b080ae7881e6a03bab
Alex Bradley <a.bradley.dm@gmail.com>
2021-12-23 19:07:03 +0000

    Day 21 P2

--- Y2021/Day21.hs
+++ Y2021/Day21.hs
@@ -1,5 +1,11 @@
 module AdventOfCode.Y2021.Day21 where
 
+import AdventOfCode.Common.Tuple
+import Data.Maybe
+
+import Data.Map (Map)
+import qualified Data.Map as M
+
 data Player = Player
    { ident :: Int
    , position :: Int
@@ -17,6 +23,9 @@ data Game = Game
    , player2 :: Player
    , die :: DeterministicDie
    } deriving (Show, Eq)
+
+type Wins = (Integer,Integer)
+(a,b) <+> (c,d) = (a+c, b+d)
 
 --------------------------------------------------------------------------------
 
@@ -59,6 +68,50 @@ part1 game = let
    loserScore = min (score $ player1 endState) (score $ player2 endState)
    dieRolls = totalRolls $ die endState
    in loserScore * dieRolls
+
+--------------------------------------------------------------------------------
+
+quantumDieRoll :: [Int]
+quantumDieRoll = [1,2,3]
+
+winner2 :: Player -> Bool
+winner2 player = score player >= winningScore
+
+winningScore = 21
+
+turnTheTables :: Game -> Game
+turnTheTables game = game { player1 = player2 game, player2 = player1 game }
+
+doGame2 :: Game -> Wins
+doGame2 = aux' where
+   
+   mem :: Map (Int,Int,Int,Int) Wins
+   mem = M.fromList $ do 
+      p1s <- [0.. winningScore + 9]
+      p1p <- [1..10]
+      p2s <- [0.. winningScore + 9]
+      p2p <- [1..10]
+      return ((p1p,p1s,p2p,p2s), aux $ Game (Player 1 p1p p1s) (Player 2 p2p p2s) undefined)
+   
+   aux :: Game -> Wins
+   aux game
+      | winner2 (player1 game) = (1,0)
+      | winner2 (player2 game) = (0,1)
+      | otherwise = let
+         rolls = [(3,1), (4,3), (5,6), (6,7), (7,6), (8,3), (9,1)]
+         advancedPlayers = map (\(roll,freq) -> (advance roll (player1 game), freq)) rolls
+         outcomes = map (\(ap,freq) -> (*freq) $# (aux' $ turnTheTables $ game {player1 = ap})) advancedPlayers
+         in swap $ foldr1 (<+>) outcomes
+   
+   aux' :: Game -> Wins
+   aux' game = let
+      p1 = player1 game
+      p2 = player2 game
+      key = (position p1, score p1, position p2, score p2)
+      in fromJust $ M.lookup key mem
+
+part2 :: Game -> Integer
+part2 game = uncurry max $ doGame2 game
 
 --------------------------------------------------------------------------------
 
